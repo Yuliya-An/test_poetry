0c75d4dc2e40d626e3d28a4c2f82ff277e8a57a1
Yuliya A <juljuly@ya.ru>
2026-01-05 19:30:17 +0300

    fix: add type hints, docstrings, fix linting

--- src/processing.py
+++ src/processing.py
@@ -1,26 +1,29 @@
-def filter_by_state(data, state='EXECUTED'):
-    """Фильтрует по статусу (по умолчанию EXECUTED)"""
+from typing import Any, Dict, List
+
+
+def filter_by_state(transactions: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
+    """Фильтрует банковские операции по статусу (по умолчанию EXECUTED)."""
     result = []
-    for operation in data:
-        if operation.get('state') == state:
-            result.append(operation)
+    for transaction in transactions:
+        if transaction.get("state") == state:
+            result.append(transaction)
     return result
 
 
-def sort_by_date(data, reverse=True):
-    """Сортирует по дате (по умолчанию - новые сверху)"""
-    return sorted(data, key=lambda x: x['date'], reverse=reverse)
+def sort_by_date(transactions: List[Dict[str, Any]], reverse_sort: bool = True) -> List[Dict[str, Any]]:
+    """Сортирует банковские операции по дате (по умолчанию новые сверху)."""
+    return sorted(transactions, key=lambda t: t["date"], reverse=reverse_sort)
 
 
-# # Тесты (закомментировать перед сдачей)
-# if __name__ == "__main__":
-#     data = [
-#         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-#     ]
-#
-#     print("EXECUTED:", filter_by_state(data))
-#     print("CANCELED:", filter_by_state(data, 'CANCELED'))
-#     print("СОРТИРОВКА:", sort_by_date(data))
+# Тесты (закомментировать перед сдачей)
+if __name__ == "__main__":
+    data = [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+    print("EXECUTED:", filter_by_state(data))
+    print("CANCELED:", filter_by_state(data, "CANCELED"))
+    print("СОРТИРОВКА:", sort_by_date(data))
