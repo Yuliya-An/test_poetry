37eb54b8e9c0a085cc120d94bd3dd24a0a636efd
Yuliya A <juljuly@ya.ru>
2026-01-18 19:40:19 +0300

    test: add tests for masks module

---
+++ tests/test_masks.py
@@ -0,0 +1,15 @@
+import pytest
+from src.masks import get_mask_card_number, get_mask_account
+
+@pytest.mark.parametrize("card, expected", [
+    ("7000792289606361", "7000 79** **** 6361"),
+    ("1596837868705199", "1596 83** **** 5199"),
+])
+def test_mask_card(card, expected):
+    assert get_mask_card_number(card) == expected
+
+@pytest.mark.parametrize("account, expected", [
+    ("73654108430135874305", "**4305"),
+])
+def test_mask_account(account, expected):
+    assert get_mask_account(account) == expected
